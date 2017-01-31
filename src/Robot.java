f765c279f10903079c9ac7329d7ffae49d27c694
qianjinpeixun <jin.qian.canada@gmail.com>
2017-01-31 16:31:32 -0400

    update comments

--- src/Robot.java
+++ src/Robot.java
@@ -1,11 +1,167 @@
 
+/**
+ * The objective is to build a simple robot game where the robots are modeled as
+ * objects and move around in a 2- dimensional grid as shown in the diagram
+ * below. The position of the robot is expressed as x- and ycoordinates, which
+ * can take any value within the limits of the grid. The goal of the game is to
+ * move the robot so that reaches the grid number (8,8) from grid number (1,1).
+ * The robot also faces one of three directions (Up, Right or Diagonal). It can
+ * move one, two or three steps in the direction it faces. It can also collect
+ * points as it moves across the grid.
+ * 
+ * @author ST1
+ *
+ */
 
 public class Robot {
 
 	private String name = "";
 	private int x = 1;
 	private int y = 1;
-	private int direction=1;
+	private int direction = 1;
+	private int collectedPoints;
+
+	/**
+	 * 
+	 * Constructor that sets the name and the direction it faces to user-given
+	 * values. The constructor should also set the x and y positions to (1,1)
+	 * and the points collected to 0.
+	 * 
+	 * @param name
+	 * @param direction
+	 */
+	public Robot(String name, String direction) {
+		this.name = name;
+		if (direction.equals("Up"))
+			this.direction = 1;
+		else if (direction.equals("Right"))
+			this.direction = 2;
+		else if (direction.equals("Diagonal"))
+			this.direction = 3;
+		else // if the input is incorrect, set default direction to Up
+			this.direction = 1;
+
+		this.x = 1;
+		this.y = 1;
+		this.collectedPoints = 0;
+
+	}
+
+	/**
+	 * According to the steps, calculate the next X point, need to considerate
+	 * the direction
+	 * 
+	 * @param steps
+	 * @return
+	 */
+	public int getNextX(int steps) {
+		int newX = x;
+		if (direction == 1) {
+			newX = x;
+		} else if (direction == 2) {
+			newX = x + steps;
+		} else {
+			newX = x + steps;
+		}
+		return newX;
+	}
+
+	/**
+	 * According to the steps, calculate the next Y point, need to considerate
+	 * the direction
+	 * 
+	 * @param steps
+	 * @return
+	 */
+	public int getNextY(int steps) {
+		int newY = y;
+		if (direction == 1) {
+			newY = y + steps;
+		} else if (direction == 2) {
+			newY = y;
+		} else {
+			newY = y + steps;
+		}
+		return newY;
+	}
+
+	/**
+	 * 
+	 * moves the robot by the given number of steps in the direction it faces.
+	 * 
+	 * @param steps
+	 * @param direction
+	 */
+	public void move(int steps) {
+		if (direction == 1) {
+			y = y + steps;
+		} else if (direction == 2) {
+			x = x + steps;
+		} else {
+			y = y + steps;
+			x = x + steps;
+		}
+		if (x > 8)
+			x = 8;
+		if (y > 8)
+			y = 8;
+		collectedPoints = collectedPoints + x + y;
+	}
+
+	
+	
+
+	/**
+	 * 
+	 * compares the x and y coordinates of this robot with the x and y
+	 * coordinates of another robot and returns true if this robot is closer to
+	 * (8,8). A simple test is to just add the x and y values and see which is
+	 * larger. For example, a robot at (3,3) is ahead than the robot at (1,4).
+	 * As another example, a robot at (7,1) and another at (4,4) are at the same
+	 * distance from (8,8).
+	 * 
+	 * @param anotherRobot
+	 * @return
+	 */
+	public boolean AmIAhead(Robot anotherRobot) {
+		boolean ret = false;
+		int myDistance = this.x + this.y;
+		int anohterDistance = anotherRobot.x + anotherRobot.y;
+		if (myDistance > anohterDistance)
+			ret = true;
+		else
+			ret = false;
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		String nameStr = String.format("%-10s", name);
+		String directionStr = "";
+		if (direction == 1)
+			directionStr = "Up";
+		else if (direction == 2)
+			directionStr = "Right";
+		else
+			directionStr = "Diagonal";
+
+		directionStr = String.format("%-8s", directionStr);
+
+		return nameStr + " (" + x + "," + y + ") " + directionStr + "    " + collectedPoints + " points";
+	}
+
+	public String getMoveFailMessage() {
+		String nameStr = String.format("%-10s", name);
+		String directionStr = String.format("%-8s", direction);
+
+		return nameStr + " (" + x + "," + y + ") move fails   " + collectedPoints + " points";
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
 	public int getX() {
 		return x;
 	}
@@ -36,104 +192,6 @@ public class Robot {
 
 	public void setCollectedPoints(int collectedPoints) {
 		this.collectedPoints = collectedPoints;
-	}
-
-	private int collectedPoints;
-
-	public Robot(String name, String direction) {
-		this.name = name;
-		if(direction.equals("Up")) this.direction=1;
-		else 		if(direction.equals("Right")) this.direction=2;
-		else 		if(direction.equals("Diagonal")) this.direction=3;
-
-		this.x = 1;
-		this.y = 1;
-		this.collectedPoints = 0;
-		
-	}
-	
-	public int getNextX(int steps, int direction){
-		int newX=x;
-		if(direction==1){
-			newX= x;
-		}else if(direction ==2){
-			newX=x+steps;
-		}else{
-			newX=x+steps;
-		}
-		return newX;
-	}
-	
-	
-	public int getNextY(int steps, int direction){
-		int newY=y;
-		if(direction==1){
-			newY= y+steps;
-		}else if(direction ==2){
-			newY=y;
-		}else{
-			newY=y+steps;
-		}
-		return newY;
-	}
-
-	
-	
-
-	public void move(int steps,int direction) {
-		if(direction==1){
-			y=y+steps;
-		}else if(direction ==2){
-			x=x+steps;
-		}else{
-			y=y+steps;
-			x=x+steps;
-		}
-		if(x>8) x=8;
-		if(y>8) y=8;
-		this.collectedPoints =collectedPoints+x+y;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public static void main(String[] args) {
-
-	}
-
-	public boolean AmIAhead(Robot anotherRobot) {
-		boolean ret = false;
-		int myDistance = this.x + this.y;
-		int anohterDistance = anotherRobot.x + anotherRobot.y;
-		if (myDistance > anohterDistance)
-			ret = true;
-		else
-			ret = false;
-		return ret;
-	}
-
-	@Override
-	public String toString() {
-		String nameStr=String.format("%-10s", name);
-		String directionStr="";
-		if(direction==1) directionStr="Up";
-		else if(direction==2) directionStr="Right";
-		else directionStr="Diagonal";
-		
-		directionStr=String.format("%-8s", directionStr);
-		
-		
-		return nameStr+" ("+x+","+y+") "+directionStr+"    "+collectedPoints+" points";
-	}
-	
-	
-	public String getMoveFailMessage() {
-		String nameStr=String.format("%-10s", name);
-		String directionStr=String.format("%-8s", direction);
-		
-		
-		return nameStr+" ("+x+","+y+") move fails   "+collectedPoints+" points";
 	}
 
 }
--- src/RobotGame.java
+++ src/RobotGame.java
@@ -9,7 +9,7 @@ public class RobotGame {
 		try {
 			System.out.println("Welcome to the Robot Game Simulator!");
 
-			System.out.println("Enter the name of the first robot:");
+			System.out.print("Enter the name of the first robot:");
 			InputStreamReader in = new InputStreamReader(System.in);
 			BufferedReader br = new BufferedReader(in);
 			String firstName = "";
@@ -18,11 +18,11 @@ public class RobotGame {
 				if (firstName != null && firstName.length() < 10) {
 					break;
 				} else {
-					System.out.println("Enter the name of the first robot, maximum 10 letters!");
+					System.out.print("Enter the name of the first robot, maximum 10 letters:");
 				}
 			}
 
-			System.out.println("Enter the direction of the first robot: Up, Right or Diagonal");
+			System.out.print("Enter the direction of the first robot: Up, Right or Diagonal:");
 			String firstDirection = "";
 			while (true) {
 				firstDirection = br.readLine();
@@ -30,24 +30,24 @@ public class RobotGame {
 						|| firstDirection.equals("Diagonal"))) {
 					break;
 				} else {
-					System.out.println("Enter the direction of the first robot: Up, Right or Diagonal");
+					System.out.print("Enter the direction of the first robot: Up, Right or Diagonal:");
 				}
 			}
 			Robot firstRobot = new Robot(firstName, firstDirection);
 			// System.out.println("First robot has been created:" + firstRobot);
 
-			System.out.println("Enter the name of the second robot:");
+			System.out.print("Enter the name of the second robot:");
 			String secondName = "";
 			while (true) {
 				secondName = br.readLine();
 				if (secondName != null && secondName.length() < 10) {
 					break;
 				} else {
-					System.out.println("Enter the name of the second robot, maximum 10 letters!");
+					System.out.print("Enter the name of the second robot, maximum 10 letters:");
 				}
 			}
 
-			System.out.println("Enter the direction of the second robot: Up, Right or Diagonal");
+			System.out.print("Enter the direction of the second robot: Up, Right or Diagonal:");
 			String secondDirection = "";
 			while (true) {
 				secondDirection = br.readLine();
@@ -55,25 +55,22 @@ public class RobotGame {
 						|| secondDirection.equals("Diagonal"))) {
 					break;
 				} else {
-					System.out.println("Enter the direction of the second robot: Up, Right or Diagonal");
+					System.out.print("Enter the direction of the second robot: Up, Right or Diagonal:");
 				}
 			}
 			Robot secondRobot = new Robot(secondName, secondDirection);
-			// System.out.println("Second robot has been created:" +
-			// secondRobot);
-
+		
 			firstRobot.setCollectedPoints(2);
 			secondRobot.setCollectedPoints(2);
 
-			// System.out.println("firstRobotMoveNumber="+firstRobotMoveNumber);
-			// System.out.println("secondRobotMoveNumber="+secondRobotMoveNumber);
-
+			System.out.println("");
 			System.out.println("Simulation started!");
 			System.out.println(firstRobot);
 			System.out.println(secondRobot);
 
 			boolean isFirstReach = false;
 			boolean isSecondReach = false;
+			
 			boolean hasFirstQuit=false;
 			boolean hasSecondQuit=false;
 			int newSteps = 1;
@@ -89,14 +86,16 @@ public class RobotGame {
 					newSteps = (int) (Math.random() * 3 + 1);
 					newDirection = (int) (Math.random() * 3 + 1);
 					firstRobot.setDirection(newDirection);
-					int nextX=firstRobot.getNextX(newSteps,newDirection);
-					int nextY=firstRobot.getNextY(newSteps,newDirection);
+					
+					
+					int nextX=firstRobot.getNextX(newSteps);
+					int nextY=firstRobot.getNextY(newSteps);
 					
 					if(nextX==secondRobot.getX()&& nextY==secondRobot.getY()){
 						firstRobot.setCollectedPoints(firstRobot.getCollectedPoints()-(nextX+nextY));
 						System.out.println(firstRobot.getMoveFailMessage());
 					}else{
-						firstRobot.move(newSteps, newDirection);
+						firstRobot.move(newSteps);
 						System.out.println(firstRobot);
 					}
 					if (firstRobot.getX() == 8 && firstRobot.getY() == 8) {
@@ -108,14 +107,14 @@ public class RobotGame {
 					newDirection = (int) (Math.random() * 3 + 1);
 					
 					secondRobot.setDirection(newDirection);
-					int nextX=secondRobot.getNextX(newSteps,newDirection);
-					int nextY=secondRobot.getNextY(newSteps,newDirection);
+					int nextX=secondRobot.getNextX(newSteps);
+					int nextY=secondRobot.getNextY(newSteps);
 					
 					if(nextX==firstRobot.getX()&& nextY==firstRobot.getY()){
 						secondRobot.setCollectedPoints(secondRobot.getCollectedPoints()-(nextX+nextY));
 						System.out.println(secondRobot.getMoveFailMessage());
 					}else{
-						secondRobot.move(newSteps, newDirection);
+						secondRobot.move(newSteps);
 						System.out.println(secondRobot);
 					}
 					
